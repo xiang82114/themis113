ffe6382642518b763dfe5b55569008089159183b
Xavier <xiang82114@gmail.com>
2025-06-02 21:59:09 +0800

    加入色系css 調整 新增流程頁面

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,3 @@
-window.addEventListener("DOMContentLoaded", function () {
-    // 1 秒後加上 hide class 讓遮罩淡出
-    setTimeout(function () {
-    document.getElementById("splash").classList.add("hide");
-    }, 1000); // 動畫 1 秒
-});
-
 document.body.id = "skrollr-body"
 $(window).scroll(
     function(evt){
