9fc770bd7511734dd4c37b84ba4815b168c5c1aa
Xavier <xcc82114@gmail.com>
2025-08-03 22:02:08 +0800

    更新

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,22 @@
-document.body.id = "skrollr-body"
-$(window).scroll(
-    function(evt){
-        if($(window).scrollTop()>0)
-            $(".navbar").removeClass("navbar-top");
-        else
-            $(".navbar").addClass("navbar-top");
-    }
-);
+// document.body.id = "skrollr-body";
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         if ($(window).scrollTop() > 0) {
+//             $(".navbar").removeClass("navbar-top");
+//             document.getElementById("logo").src = "./imges/圖片2.png";
+//         } else {
+//             $(".navbar").addClass("navbar-top");
+//             document.getElementById("logo").src = "./imges/圖片1.png";
+//         }
+//     });
+// });
+// var s = skrollr.init();
 
-var s = skrollr.init();
+window.addEventListener('scroll', function () {
+  const nav = document.querySelector('.navbar-top');
+  if (window.scrollY > 10) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
